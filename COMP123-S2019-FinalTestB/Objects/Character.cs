fd862efb4cbffb385237b832248a42342967645a
Mushfika Farzana <mfarzan1@my.centennialcollege.ca>
2019-08-12 12:53:01 -0400

    Added Character and Item classes

---
+++ COMP123-S2019-FinalTestB/Objects/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student Name: Mushfika Farzana
+ * Student ID: 301051702
+ * Description: This is the Character class used in character creation
+               This is also the Data container for the application
+  */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Character
+    {
+        // Character Abilities
+        public string Strength { get; set; }
+        public string Dexterity { get; set; }
+        public string Constitution { get; set; }
+        public string Intelligence { get; set; }
+        public string  Wisdom { get; set; }
+        public string Charisma { get; set; }
+
+        // Secondary Abilities
+        public int ArmourClass { get; set; }
+        public int HitPoints { get; set; }
+
+        //Character Class
+        public string CharaacterClass { get; set; }
+        public int Level { get; set; }
+
+        // Equipment
+        List<Item> Inventory;
+
+        // Constructor 
+        Character()
+        {
+            this.Inventory = new List<Item>();
+        }
+    }
+}
---
+++ COMP123-S2019-FinalTestB/Objects/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Student Name: Mushfika Farzana
+ * Student ID: 301051702
+ * Description: This is the Item class
+                This is used for Inventory Management
+  */
+
+namespace COMP123_S2019_FinalTestB.Objects
+{
+    class Item
+    {
+        public string Description { get; set; }
+        public float Weight { get; set; }
+        public int Cost { get; set; }
+    }
+}
